25f93870a879bcdebd575f34c26cb4e5bbfee00f
Cory Bass <cory.bass.07@gmail.com>
2020-10-05 11:35:34 -0500

    updated notes on JS

---
+++ javascript/notes.js
@@ -0,0 +1,23 @@
+// // swap variable values with variable deconstruction
+
+// let playerOne = 'Tiffany';
+// let playerTwo = 'Kristine';
+
+// [playerOne, playerTwo] = [playerTwo, playerOne];
+
+// // implement array destructuring
+
+// const apiList = [
+//     'api1',
+//     'api2',
+//     'api3'
+// ]
+
+// const [posts, repos, profile] = apiList;
+
+// console.log(posts);
+// console.log(repos);
+// console.log(profile);
+
+// Pass JavaScript Objects as Function Arguments by Leveraging Deconstruction
+
