7a6a0fc556e77ef84f52ea5b648aebbe8259c006
Cory Bass <cory.bass.07@gmail.com>
2020-10-05 17:15:15 -0500

    added section on fetch calls and promises

---
+++ javascript/fetchpromise.js
@@ -0,0 +1,31 @@
+const postsPromise = fetch("https://api.dailysmarty.com/posts");
+// console.log("Starting fetch call");
+// console.log("After fetch call");
+// console.log(postsPromise);
+// console.log("After program has run");
+
+postsPromise
+    .then(data => data.json())
+    .then(data => {
+        data.posts.forEach((item) => {
+          console.log(item.url_for_post);
+        });
+    })
+.catch((err) => {
+  console.log(err);
+});
+
+// // http is unsecure and returns a bad fetch call
+
+// const postsPromise = fetch("http://api.dailysmarty.com/posts");
+
+// postsPromise
+//     .then(data => data.json())
+//     .then(data => {
+//         data.posts.forEach((item) => {
+//           console.log(item.url_for_post);
+//         });
+//     })
+// .catch((err) => {
+//   console.log(err);
+// });
