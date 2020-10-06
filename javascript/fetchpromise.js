587d9fd99789b5bf4a8550e3115ea71b9540fe59
Cory Bass <cory.bass.07@gmail.com>
2020-10-06 11:05:20 -0500

    added section on multiple api async and errors

--- javascript/fetchpromise.js
+++ javascript/fetchpromise.js
@@ -5,15 +5,15 @@ const postsPromise = fetch("https://api.dailysmarty.com/posts");
 // console.log("After program has run");
 
 postsPromise
-    .then(data => data.json())
-    .then(data => {
-        data.posts.forEach((item) => {
-          console.log(item.url_for_post);
-        });
-    })
-.catch((err) => {
-  console.log(err);
-});
+  .then((data) => data.json())
+  .then((data) => {
+    data.posts.forEach((item) => {
+      console.log(item.url_for_post);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // // http is unsecure and returns a bad fetch call
 
